e95bcb71835f3aaf62dd89883de76ece61eae831
Joe Greiner <jrgreiner115@gmail.com>
2018-03-22 17:53:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 # Add  code here!
-def prime?(number)
-  2.upto(Math.sqrt(number)) do |x| 
-    if number % x == 0
-      return false 
-    else
-      return true 
-  end
+def prime?(num)
+    2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil   
+    else return false
 end
 end
